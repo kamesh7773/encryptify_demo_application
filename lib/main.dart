b94123f82daa35161f1bad7ed048c97bb68e0209
kamesh singh <88535029+kamesh7773@users.noreply.github.com>
2025-02-17 16:59:28 +0530

    Update MyApp and MyHomePage to reflect Encryptify branding

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(title: 'Encryptify Example', home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -47,10 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text("Encryptify Demo App")),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
