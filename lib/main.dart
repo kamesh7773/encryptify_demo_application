9913a4287fc96d594bf6108f3333fcbeb9cf8bb4
kamesh singh <88535029+kamesh7773@users.noreply.github.com>
2025-02-17 16:13:50 +0530

    Add encryption functionality and integrate secure storage plugins

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:encryptify/encryptify.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  EncryptionDecryption.generateKeys(); // Generate RSA keys
+
   runApp(const MyApp());
 }
 
@@ -48,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
